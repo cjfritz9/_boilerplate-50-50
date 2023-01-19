e3054402bcbe5436f02b4f669b521612cc8d0806
CJ <cjfritz9@gmail.com>
2023-01-18 19:07:20 -0700

    initial

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as Router from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <Router.Routes>
+        <Router.Route path='/' element={} />
+      </Router.Routes>
+    </div>
+  );
+};
+
+export default App;
